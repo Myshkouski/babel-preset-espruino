a946dd1e6cd1ec22aa52018576224bbb60c87a68
Alexei Myshkouski <alexeimyshkouski@gmail.com>
2018-12-13 00:15:16 +0300

    Based on https://github.com/andrewwakeling/espruino-webpack-babel-sample

---
+++ index.js
@@ -0,0 +1,35 @@
+var plugins = [
+	['@babel/plugin-proposal-object-rest-spread', {
+		// useBuiltIns: true,
+		// loose: true
+	}],
+	'@babel/plugin-transform-block-scoped-functions',
+	['@babel/plugin-transform-block-scoping', {
+		throwIfClosureRequired: true
+	}],
+	['@babel/plugin-transform-spread', {
+		// loose: true
+	}],
+	['@babel/plugin-transform-destructuring', {
+		// useBuiltIns: true,
+		// loose: true
+	}],
+	'@babel/plugin-transform-parameters',
+	'@babel/plugin-transform-shorthand-properties',
+	['module-resolver', {
+		alias: {
+			events: '@alexeimyshkouski/espruino-events'
+		}
+	}],
+	['module:fast-async', {
+		compiler: {
+			noRuntime: true
+		}
+	}]
+]
+
+module.exports = function() {
+	return {
+		plugins: plugins
+	}
+}
